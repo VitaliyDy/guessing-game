7364414ba4a766f60af9c66ef5272f78106ac1d8
Vitaliy Dybov <VitaliyDybov@gmail.com>
2017-09-16 23:30:24 +0300

    First attempt with indexes of range array

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.range = [];
+        this.maxIndex;
+        this.minIndex = 0;
+        this.candidateIndex;
+    }
+    
     setRange(min, max) {
-
+        //let range = [];
+        this.maxIndex = max - min;
+        this.range[0] = min;
+        for (var i = 1; i <= max; i++) {
+            this.range[i] = min + i;
+        }      
     }
 
-    guess() {
-
+    guess() {        
+        this.candidateIndex = Math.ceil((this.maxIndex+this.minIndex)/2)
+        return this.range[this.candidateIndex];
     }
 
     lower() {
-
+        this.maxIndex = this.candidateIndex;        
     }
 
     greater() {
-
+        this.minIndex = this.candidateIndex;
     }
 }
 
